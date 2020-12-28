705949663173cdd7144031a59d9a285273464b85
Max Holman <max@holmn.com>
2020-12-28 22:39:22 +0800

    fix: add triple-slash refs

--- src/opa.js
+++ src/opa.js
@@ -1,6 +1,10 @@
 // Copyright 2018 The OPA Authors.  All rights reserved.
 // Use of this source code is governed by an Apache2
 // license that can be found in the LICENSE file.
+
+/// <reference lib="es5" />
+/// <reference lib="dom" />
+
 const builtIns = require("./builtins/index");
 
 /**
